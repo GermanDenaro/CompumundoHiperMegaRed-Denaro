aa19d85af9b6002b28b99b8de11068e42e4c2045
German] <germandenaroc@gmail.com>
2021-05-30 18:27:08 -0300

    Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-    return (
-        <div className='footer text-center py-3'>
-            <div>
-                &copy; {new Date().getFullYear()} Copyright: CompumundoHyperMegaRed
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="footer text-center py-3">
+      <div>
+        &copy; {new Date().getFullYear()} Copyright: CompumundoHyperMegaRed
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -18,27 +18,28 @@ const ItemDetail = ({ item }) => {
   // console.log(itemDetailed);
 
   return (
-    <div className="container mt-5 animate__animated animate__fadeIn">
-      <Row>
+    <div className="container mt-4 animate__animated animate__fadeIn pb-5">
+      <Row className="border border-dark rounded rounded-lg mb-3">
         <Col
           md={7}
           className="mr-2 animate__animated animate__fadeInLeft animate__faster text-center"
         >
           <Image src={item.pictureUrl} fluid className="image-detail" />
         </Col>
-        <Col className="animate__animated animate__fadeInRight animate__faster bg-light mb-3 rounded-lg">
+        <Col className="animate__animated animate__fadeInRight animate__faster bg-light border-left border-dark rounded-right">
           <h1 className="text-center mt-3">{item.title}</h1>
-          <p className="text-center">{item.extendedDesc}</p>
-          <p className="text-center">
-            <AttachMoneyIcon className="dollar-icon" />
+          <p>{item.extendedDesc}</p>
+          <p className="text-center item-price">
+            <AttachMoneyIcon fontSize="large" className="dollar-icon" />
             {item.price}
           </p>
           <ItemCount items={item} onAdd={onAdd} />
         </Col>
       </Row>
-      <div className="text-center">
-        <h1>Especificaciones del producto</h1>
-        {item.specs && item.specs.map((spec) => <p>{spec}</p>)}
+      <div className="text-center border border-dark rounded rounded-lg spec-bg">
+        <h1 className="mt-2 spec-title">Especificaciones del producto</h1>
+        {item.specs &&
+          item.specs.map((spec) => <p className="spec-text">{spec}</p>)}
       </div>
     </div>
   );
